f3135817548330ec8c1f1931b2508c6d9c18e8ec
Vivian <>
2020-03-28 13:54:05 -0400

    testNG & testing

--- src/main/java/org/vivan/App.java
+++ src/main/java/org/vivan/App.java
@@ -8,6 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(getMessage("fred"));
+    }
+    
+    public static String getMessage(String name) {
+    	if (null == name || 0 == name.length()) {
+    		return "Hello!";
+    	} else {
+    		return "Hello " + name.substring(0, 1).toUpperCase() +name.substring(1) + "!";
+    	}
     }
 }
--- src/test/java/org/vivan/AppTest.java
+++ src/test/java/org/vivan/AppTest.java
@@ -1,20 +1,38 @@
 package org.vivan;
 
-import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
 
-import org.junit.Test;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
+
+	@Test
+    public void instantiationTest()
     {
-        assertTrue( true );
+        new App();
     }
+	
+	@Test(dataProvider = "getMessageTestDP")
+    public void getMessageTest(String in, String expected)
+    {
+		assertEquals(App.getMessage(in), expected);
+    }
+
+	@DataProvider
+	public Object[][] getMessageTestDP() {
+		return new Object[][] {
+			{null, "Hello!"},
+			{"", "Hello!"},
+			
+			{"s", "Hello S!"},
+			
+			{"bob", "Hello Bob!"},
+			{"fred", "Hello Fred!"},
+		};
+	}
 }
