9db9b63c5f4de3ab958306c568ea820be2a9516a
Vivian Rose <>
2020-04-18 19:00:28 -0400

    prints name 10 times

---
+++ src/main/java/org/vivan/tests/PrintNames.java
@@ -0,0 +1,10 @@
+package org.vivan.tests;
+
+public class PrintNames {
+	public static void main(String args[]){
+		int name;
+		for(int name1 = 1;name1<=10;name1++){
+			System.out.println("vivian");
+		}
+	}
+}
