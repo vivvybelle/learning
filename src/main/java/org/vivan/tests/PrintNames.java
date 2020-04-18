ebeb25416dd2da41d7b1de182a68dbed196eb49a
Vivian Rose <>
2020-04-18 19:03:11 -0400

    improved readablity

--- src/main/java/org/vivan/tests/PrintNames.java
+++ src/main/java/org/vivan/tests/PrintNames.java
@@ -1,10 +1,15 @@
 package org.vivan.tests;
 
 public class PrintNames {
-	public static void main(String args[]){
-		int name;
-		for(int name1 = 1;name1<=10;name1++){
+
+	public static void main(String args[]) {
+		printNames();
+	}
+	
+	public static void printNames() {
+		for(int i = 0; i < 10; i++) {
 			System.out.println("vivian");
 		}
 	}
+
 }
