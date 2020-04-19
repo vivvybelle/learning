29726785ec715c80faf978eb1fef65aac06d04cb
Vivian Rose <>
2020-04-18 20:03:41 -0400

    prints out vivian 1-10

--- src/main/java/org/vivan/tests/PrintNames.java
+++ src/main/java/org/vivan/tests/PrintNames.java
@@ -4,12 +4,20 @@ public class PrintNames {
 
 	public static void main(String args[]) {
 		printNames();
+		printNamesWithNumbers();
 	}
 	
 	public static void printNames() {
-		for(int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++) {
 			System.out.println("vivian");
 		}
 	}
+
+	public static void printNamesWithNumbers() {
+		for (int i = 0; i < 10;  i++) {
+			System.out.println("vivian " + (i + 1));
+		}
+	}
+
 
 }
