cb407acae4c41af5eb502ca90e627cc62828645f
Vivian Rose <>
2020-05-02 19:50:24 -0400

    I made a divider object

---
+++ src/test/java/org/vivan/tests/DividerTest.java
@@ -0,0 +1,32 @@
+package org.vivan.tests;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DividerTest {
+
+    @Test(dataProvider="divideTestDP")
+    public void subtractTest(int init, int divitend, int expected) {
+        Divider d = new Divider(init);
+        assertEquals(d.divide(divitend), expected);
+    }
+    
+    @DataProvider
+    public Object[][] divideTestDP() {
+        return new Object[][] {
+            {0, 8, 0},
+            {0, 1, 0},
+            {0, 5, 0},
+            
+            {8, 1, 8},
+            {4, 2, 2},
+            {8, 2, 4},
+
+            {-2, 1, -2},
+            {-5, 5, -1},
+            {-6, 3, -2},
+        };
+    }
+}
