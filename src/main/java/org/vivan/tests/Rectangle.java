fe62b6def422f6cd52f70cf5ae0c75d759fa43a5
Vivian Rose <>
2020-05-06 20:40:13 -0400

    i fixed a rectangle object

--- src/main/java/org/vivan/tests/Rectangle.java
+++ src/main/java/org/vivan/tests/Rectangle.java
@@ -4,12 +4,14 @@ import lombok.Value;
 
 @Value
 public class Rectangle {
-    int length,width;
+    int length;
+    int width;
 
     public int area() {
         return length * width;
     }
-    public int perimeter(){
-        return length + length + width + width;
+
+    public int perimeter() {
+        return (length * 2) + (width * 2);
     }
 }
